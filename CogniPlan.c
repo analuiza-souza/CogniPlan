f05e6eab6fb3903cfa086338763b6aa51347127a
Ana Luiza Souza <annaluh107@gmail.com>
2026-09-21 23:06:54 -0300

    adiciona cadastro e listagem de tarefas por disciplina

--- CogniPlan.c
+++ CogniPlan.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 void limparBuffer() {
     int c;
@@ -8,10 +7,11 @@ void limparBuffer() {
 
 
 int main() {
-    char    materias[10][50];
+    char    materias[50][50];
     int     quantidadeMaterias = 0;
-    char    tarefas[20][50];
+    char    tarefas[50][50];
     int     quantidadesTarefas = 0;
+    char    tarefasMaterias[50][50];
     int     opcao;
 
     do {
@@ -20,6 +20,7 @@ int main() {
         printf("1 - Cadastrar materias\n");
         printf("2 - Listar materias\n");
         printf("3 - Cadastrar tarefas\n");
+        printf("4 - Listar tarefas\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
         scanf("%d", &opcao);
@@ -27,27 +28,36 @@ int main() {
     switch (opcao) {
         case 1:
             printf("Digite o nome da materia: ");
-            scanf(" %[^\n]", materias[quantidadeMaterias]); quantidadeMaterias++;
+                scanf(" %[^\n]", materias[quantidadeMaterias]); quantidadeMaterias++;
             printf("\nMateria cadastrada!\n\n");
         break;
 
         case 2:
             printf("\n\n###### MATERIAS ######\n");
-            for(int i = 0; i < quantidadeMaterias; i++) {
+                for(int i = 0; i < quantidadeMaterias; i++) {
             printf("%d.%s\n", i + 1, materias[i]); 
     }
         break;
 
         case 3:
-        printf("Digite a tarefa: ");
-        scanf(" %[^\n]", tarefas[quantidadesTarefas]); quantidadesTarefas++;
-        printf("Tarefa cadastrada!\n\n");
+            printf("Digite a tarefa: ");
+                scanf(" %[^\n]", tarefas[quantidadesTarefas]);
+
+            printf("Digite a materia: ");
+                scanf(" %[^\n]", tarefasMaterias[quantidadesTarefas]); quantidadesTarefas++;
+            printf("Tarefa cadastrada!\n");
+        break;
+
+        case 4: 
+            printf("\n\n###### TAREFAS ######\n");
+
+            for(int i = 0; i < quantidadesTarefas; i++) {
+                printf("%d.%s.[%s]\n", i + 1, tarefas[i], tarefasMaterias[i]);
+        } 
         break;
         
-
         case 0:
             printf("\nSaindo...\n");   
-
         break;
 
 
@@ -56,13 +66,11 @@ int main() {
 
     
         }
-
         if (opcao != 0) {
-            printf("Pressione Enter Se Quiser Continuar.");
+            printf("\nPressione Enter Se Quiser Continuar.");
             limparBuffer();
             getchar(); 
     }
-
     } while (opcao != 0);
 
 
